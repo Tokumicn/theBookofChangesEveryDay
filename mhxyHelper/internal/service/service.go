52bbcd4355385a89c52fdb2f3dfa5f513fc122fb
tokumi <461694355@qq.com>
2024-10-16 10:25:00 +0800

    简单实现传入价格字符串，转换为结构化数据

--- mhxyHelper/internal/service/service.go
+++ mhxyHelper/internal/service/service.go
@@ -15,7 +15,6 @@ func BuildDictByStr(dictStr string) {
 func BuildStuffByStr(stuffArr []string) error {
 
 	stuffs := make([]models.Stuff, len(stuffArr))
-
 	for _, stuf := range stuffArr {
 
 		splits := strings.Split(stuf, ",")
@@ -60,8 +59,12 @@ func BuildStuffByStr(stuffArr []string) error {
 			RegionID: region,
 		}
 
-		temp = buildVal(temp)
-
+		temp, err = buildVal(temp)
+		if err != nil {
+			// TODO log
+			fmt.Printf("buildVal [temp: %v] err: %v \n", temp, err)
+			continue
+		}
 		stuffs = append(stuffs, temp)
 	}
 
@@ -72,7 +75,30 @@ func BuildStuffByStr(stuffArr []string) error {
 
 func buildVal(s models.Stuff) (models.Stuff, error) {
 
-	if s.ValMH == 0 && s.ValRM == 0 {
-		return
+	if (s.ValMH == 0 && s.ValRM == 0) || (s.ValMH != 0 && s.ValRM != 0) {
+		// log 无需转换
+		return s, nil
 	}
+
+	if s.ValMH == 0 {
+		valMH, err := utils.RM2MH(s.ValRM)
+		if err != nil {
+			// TODO log
+			fmt.Printf("RM2MH[ValRM: %f] err: %v\n", s.ValRM, err)
+			return s, err
+		}
+		s.ValMH = valMH
+	}
+
+	if s.ValRM == 0 {
+		valRM, err := utils.MH2RM(s.ValMH)
+		if err != nil {
+			// TODO log
+			fmt.Printf("MH2RM[ValMH: %f] err: %v\n", s.ValMH, err)
+			return s, err
+		}
+		s.ValRM = valRM
+	}
+
+	return s, nil
 }
